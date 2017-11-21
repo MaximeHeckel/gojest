f5c2761a84a3fa29346305e7ef016a226f473b31
Maxime Heckel <heckelmaxime@gmail.com>
2017-11-20 19:54:44 -0800

    working binary

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
+const path = require('path');
+const fs = require('fs');
 const jest = require('jest');
-const argv = process.argv.slice(2);
 
-jest.run(argv);
+const package = require('./package.json');
+
+const defaultGoRunnerConfig = {
+  moduleFileExtensions: [
+    'go'
+  ],
+  runner: '/snapshot/' + package.name + '/node_modules/jest-runner-go',
+    testMatch: [
+      '**/?(*_)test.go'
+    ]
+}
+
+function getConfigArg(argv) {
+  if (argv.indexOf('--config') > -1 || argv.indexOf('-c') > -1) {
+    if(argv.indexOf('--config') > -1) {
+      return argv.indexOf('--config');
+    }
+    return argv.indexOf('-c');
+  } 
+  return -1;
+}
+
+function readConfigFile(file) {
+  if (file.indexOf('json') > -1) {
+    fs.readFile(file, 'utf8', function(err, data) {
+      return JSON.parse(data)
+    });
+  }
+  const data = require("./" + file)
+  return data;
+}
+
+function getRunnerPath(file) {
+  if (file.indexOf('json') > -1) {
+    fs.readFile(file, 'utf8', function(err, data) {
+      return JSON.parse(data).runner;
+    });
+  }
+  const data = require("./" + file)
+  return data.runner;
+}
+
+function validateConfigRunner(runnerPath) {
+  if (runnerPath.indexOf('/snapshot') > -1) {
+    return runnerPath;
+  }
+
+  return '/snapshot/' + package.name + '/node_modules/' + runnerPath;
+}
+
+function run() {
+  let config;
+  const argv = process.argv.slice(2);
+
+  if (getConfigArg(argv) === -1) {
+    config = JSON.stringify(defaultGoRunnerConfig);
+    argv.push(
+    '--config',
+      config
+    );
+  } /*else {
+    const configFile = readConfigFile(argv[getConfigArg(argv) + 1])
+    const runnerPath = configFile && configFile.runner;
+    const validRunnerPath = validateConfigRunner(runnerPath);
+    const tmpConfig = configFile;
+    tmpConfig.runner = validRunnerPath;
+    config = JSON.stringify(tmpConfig);
+  }*/
+
+  
+  jest.run(argv);
+}
+
+run();
---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "moduleFileExtensions": [
+    "go"
+  ],
+  "runner": "jest-runner-go",
+  "testMatch": [
+    "**/?(*_)test.go"
+  ]
+};
